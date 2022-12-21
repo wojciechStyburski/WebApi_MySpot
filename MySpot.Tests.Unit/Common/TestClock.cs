81abe35cd155396af62427db802cad8e1325bf70
bury226 <w.styburski@gmail.com>
2022-12-21 18:02:23 +0100

    Changed test conditions

--- MySpot.Tests.Unit/Common/TestClock.cs
+++ MySpot.Tests.Unit/Common/TestClock.cs
@@ -4,5 +4,5 @@ namespace MySpot.Tests.Unit.Common;
 
 public class TestClock : IClock
 {
-    public DateTime Current() => new DateTime(2022, 11, 20, 9, 0, 0);
+    public DateTime Current() => new DateTime(2022, 10, 20, 9, 0, 0);
 }
