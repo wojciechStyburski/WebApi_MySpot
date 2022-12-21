73a62786dcd6fa5a734e0004278f948f7435bb5c
bury226 <w.styburski@gmail.com>
2022-12-21 17:46:44 +0100

    Repair of tests

--- Infrastructure/DAL/DatabaseOptions.cs
+++ Infrastructure/DAL/DatabaseOptions.cs
@@ -1,6 +1,6 @@
 ﻿namespace MySpot.Infrastructure.DAL;
 
-internal sealed  class DatabaseOptions
+internal sealed class DatabaseOptions
 {
     public string ConnectionString { get; set; }
 }
--- MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
+++ MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
@@ -11,10 +11,12 @@ public class WeeklyParkingSpotTests
     #region Arrange
     private readonly Date _now;
     private readonly WeeklyParkingSpot _weeklyParkingSpot;
+    private readonly Guid _userId;
     public WeeklyParkingSpotTests()
     {
         _now = new Date(new DateTime(2022, 11, 15));
         _weeklyParkingSpot = WeeklyParkingSpot.Create(Guid.NewGuid(), new Week(_now), "P1");
+        _userId = Guid.Parse("8579BC60-CEB8-43AC-B289-1A78835DB2E0");
     }
     #endregion
 
@@ -26,7 +28,7 @@ public class WeeklyParkingSpotTests
     {
         //Arrange
         var invalidDate = DateTime.Parse(dateString);
-        var reservation = new VehicleReservation(Guid.NewGuid(), _weeklyParkingSpot.Id, "John Doe", "XYZ1234", new Date(invalidDate), 1);
+        var reservation = new VehicleReservation(Guid.NewGuid(), _userId, "John Doe", "XYZ1234", 1, new Date(invalidDate));
         
         //Act
         var exception = Record.Exception( () => _weeklyParkingSpot.AddReservation(reservation, _now));
@@ -46,9 +48,9 @@ public class WeeklyParkingSpotTests
     public void given_reservation_for_already_reserved_parking_spot_add_reservation_should_fail()
     {
         var reservationDate = _now.AddDays(1);
-        var reservation = new VehicleReservation(Guid.NewGuid(), _weeklyParkingSpot.Id, "John Doe", "ZXC123", reservationDate, 2);
+        var reservation = new VehicleReservation(Guid.NewGuid(), _userId, "John Doe", "ZXC123", 2, reservationDate);
         _weeklyParkingSpot.AddReservation(reservation, _now);
-        var nextReservation = new VehicleReservation(Guid.NewGuid(), _weeklyParkingSpot.Id, "John Doe", "ZXC123", reservationDate, 1);
+        var nextReservation = new VehicleReservation(Guid.NewGuid(), _userId, "John Doe", "ZXC123", 1, reservationDate);
 
         var exception = Record.Exception(() => _weeklyParkingSpot.AddReservation(nextReservation, reservationDate));
         
@@ -60,7 +62,7 @@ public class WeeklyParkingSpotTests
     public void given_reservation_for_not_reserved_parking_spot_add_reservation_should_succeed()
     {
         var reservationDate = _now.AddDays(1);
-        var reservation = new VehicleReservation(Guid.NewGuid(), _weeklyParkingSpot.Id, "John Doe", "ZXC123", reservationDate, 2);
+        var reservation = new VehicleReservation(Guid.NewGuid(), _userId, "John Doe", "ZXC123", 2, reservationDate);
 
         _weeklyParkingSpot.AddReservation(reservation, _now);
 
